4140120a938a120df3efa5e3dd63c8083a266d23
Rohan <rkumar7@godaddy.com>
2023-05-18 17:23:29 -0700

    testing the github action workflow with broken unit tests

--- test_functions.py
+++ test_functions.py
@@ -8,14 +8,14 @@ class TestBasicMethods(unittest.TestCase):
 
     # testing the add method
     def test_add(self):
-        self.assertEqual(add(5, 7), 12)
+        self.assertEqual(add(5, 7), 13)
         self.assertEqual(add(3, 0), 3)
         self.assertEqual(add(23, 77), 100)
-        self.assertEqual(add(35, -4), 31)
+        self.assertEqual(add(35, -4), 53)
     
     # testing the subtract method
     def test_subtract(self):
         self.assertEqual(subtract(10, 2), 8)
         self.assertEqual(subtract(0, 4), -4)
-        self.assertEqual(subtract(31, 31), 0)
-        self.assertEqual(subtract(21, -3), 24)
+        self.assertEqual(subtract(31, 31), 9)
+        self.assertEqual(subtract(21, -3), 14)
