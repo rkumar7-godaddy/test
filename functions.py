369bdfb280adb08f53230b254890b7c3455593a2
Rohan <rkumar7@godaddy.com>
2023-05-18 17:20:18 -0700

    changed command to actually run the unit test

--- functions.py
+++ functions.py
@@ -3,9 +3,14 @@
 # add method
 def add(a: int, b: int) -> int:
     # return sum of passed integers
+    # print("Adding", a, "and", b)
     return a + b
 
 # subtract method
 def subtract(a: int, b: int) -> int:
+    # print("Subtracting", a, "and", b)
     # return difference between a and b
     return a - b
+
+
+print(add(3, 5))
